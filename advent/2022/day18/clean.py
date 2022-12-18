4cd16184e8860226d684cc32f829f9ae81a2bd85
Tomo Ueda <tueda14@gmail.com>
2022-12-18 15:59:48 -0800

    clean

--- advent/2022/day18/clean.py
+++ advent/2022/day18/clean.py
@@ -1,3 +1,16 @@
 from args import example, actual
 from advent.tools import V
+
+arg = actual
+
 dirs = [(0, 1, 0), (1, 0, 0), (0, 0, 1), (-1, 0, 0), (0, -1, 0), (0, 0, -1)]
+
+# part 1
+def solve(arg):
+    lines = arg.split('\n')
+    cubes = {V(*map(int, line.split(','))) for line in lines}
+    s = 0
+    for cube in cubes:
+        s += sum([0 if cube + dir in cubes else 1 for dir in dirs])
+    return s
+print(solve(arg))
